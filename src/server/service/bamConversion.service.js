c31854e8cc04c17e054af213a673840949be84ed
Peter Javorka <peter.javorka@artin.cz>
2018-08-17 19:00:37 +0200

    BAM Conversion refactored

--- src/server/service/bamConversion.service.js
+++ src/server/service/bamConversion.service.js
@@ -5,82 +5,87 @@ import { fromScript } from '../util';
 import filename from './pipelineOutputFilename';
 
 const log = createLogger('dna:service:bamConversion');
+const { empty, defer } = Observable;
 
-export const bamConversionControlFile = (data) => {
-  if (data.skipBamConversion) {
-    return Observable.empty();
-  }
-
-  const modification = {
-    inputFile: data.controlFile,
+const conversionTypes = {
+  CONTROL_FILE: {
+    inputFile: 'controlFile',
     outputFilename: filename.bamConversion.controlFileOutput,
-    secondNameOutputFilename: filename.bamConversion.controlFileSEOutput,
-  };
-
-  const payload = Object.assign({}, data, modification);
-
-  if (data.controlFileSE && data.mutatedFileSE && data.pairEnd) {
-    const pairedPayload = Object.assign({}, data, {
-      inputFile: data.controlFileSE,
-      outputFilename: filename.bamConversion.controlFileSEOutput,
-    });
-
-    return bamConversion(Object.assign({}, payload, { secondNameOutputFilename: null }))
-      .merge(bamConversion(pairedPayload))
-      .concat(Observable.defer(() => payload.emitResult({ code: 0, operation: 'BAM Conversion - control file' })))
-      .map(info => Object.assign(info, { operation: 'BAM Conversion - control file' }))
-      .catch(err => Object.assign(err, { operation: 'BAM Conversion - control file' }));
-  }
-  return bamConversion(payload)
-    .concat(Observable.defer(() => payload.emitResult({ code: 0, operation: 'BAM Conversion - control file' })))
-    .map(info => Object.assign(info, { operation: 'BAM Conversion - control file' }))
-    .catch(err => Object.assign(err, { operation: 'BAM Conversion - control file' }));
-};
-
-export const bamConversionMutatedFile = (data) => {
-  if (data.skipBamConversion) {
-    return Observable.empty();
-  }
-
-  const modification = {
-    inputFile: data.mutatedFile,
+    inputFileSE: 'controlFileSE',
+    outputFilenameSE: filename.bamConversion.controlFileSEOutput,
+    label: 'BAM Conversion - control file',
+  },
+  MUTATED_FILE: {
+    inputFile: 'mutatedFile',
     outputFilename: filename.bamConversion.mutatedFileOutput,
-    secondNameOutputFilename: filename.bamConversion.mutatedFileSEOutput,
-  };
-
-  const payload = Object.assign({}, data, modification);
-
-  if (data.controlFileSE && data.mutatedFileSE && data.pairEnd) {
-    const pairedPayload = Object.assign({}, data, {
-      inputFile: data.mutatedFileSE,
-      outputFilename: filename.bamConversion.mutatedFileSEOutput,
-    });
-
-    return bamConversion(Object.assign({}, payload, { secondNameOutputFilename: null }))
-      .merge(bamConversion(pairedPayload))
-      .concat(Observable.defer(() => payload.emitResult({ code: 0, operation: 'BAM Conversion - mutated file' })))
-      .map(info => Object.assign(info, { operation: 'BAM Conversion - mutated file' }))
-      .catch(err => Object.assign(err, { operation: 'BAM Conversion - mutated file' }));
-  }
-
-  return bamConversion(payload)
-    .concat(Observable.defer(() => payload.emitResult({ code: 0, operation: 'BAM Conversion - mutated file' })))
-    .map(info => Object.assign(info, { operation: 'BAM Conversion - mutated file' }))
-    .catch(err => Object.assign(err, { operation: 'BAM Conversion - mutated file' }));
+    inputFileSE: 'mutatedFileSE',
+    outputFilenameSE: filename.bamConversion.mutatedFileSEOutput,
+    label: 'BAM Conversion - mutated file',
+  },
 };
 
-function bamConversion(data) {
-  const script = createScript(data);
-  return fromScript(script)
-    .do(payload => log(payload.info));
+export function bamConversionControlFile(data) {
+  return conversion(data, conversionTypes.CONTROL_FILE);
+}
+
+export function bamConversionMutatedFile(data) {
+  return conversion(data, conversionTypes.MUTATED_FILE);
+}
+
+function conversion(data, conversionType) {
+  const {
+    skipBamConversion, pairEnd, outputDirectory, controlFileSE, mutatedFileSE,
+  } = data;
+  if (skipBamConversion) {
+    return empty();
+  }
+
+  const mapFn = payload => ({ ...payload, operation: conversionType.label });
+
+  const scriptOptions = {
+    inputFile: data[conversionType.inputFile],
+    outputFilename: conversionType.outputFilename,
+    outputFilenameSE: conversionType.outputFilenameSE,
+    outputDirectory,
+    pairEnd,
+  };
+
+  const run2Scripts = pairEnd && controlFileSE && mutatedFileSE;
+
+  let observable;
+
+  if (!run2Scripts) {
+    const script = createScript(scriptOptions);
+    observable = fromScript(script);
+  } else {
+    const scriptOptions1 = { ...scriptOptions, pairEnd: false };
+    const scriptOptions2 = {
+      ...scriptOptions,
+      pairEnd: false,
+      inputFile: data[conversionType.inputFileSE],
+      outputFilename: conversionType.outputFilenameSE,
+    };
+
+    const script1 = createScript(scriptOptions1);
+    const script2 = createScript(scriptOptions2);
+
+    observable = fromScript(script1)
+      .merge(fromScript(script2));
+  }
+
+  return observable
+    .concat(defer(() => data.emitResult({ code: 0, operation: conversionType.label })))
+    .map(mapFn)
+    .catch(mapFn)
+    .do(({ info }) => log(info));
 }
 
 function createScript(data) {
   const command = 'bamToFastq';
   const outputFile = path.join(data.outputDirectory, data.outputFilename);
   let params = ['-i', data.inputFile, '-fq', outputFile];
-  if (data.pairEnd && data.secondNameOutputFilename) {
-    const secondEndFile = path.join(data.outputDirectory, data.secondNameOutputFilename);
+  if (data.pairEnd) {
+    const secondEndFile = path.join(data.outputDirectory, data.outputFilenameSE);
     params = params.concat(['-fq2', secondEndFile]);
   }
   return { command, params };
